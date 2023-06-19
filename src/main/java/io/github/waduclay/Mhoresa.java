706eba649888dc1985de1f81755ecada57884932
willdomkahari <drclae@gmail.com>
2023-06-19 11:07:43 +0200

    init

---
+++ src/main/java/io/github/waduclay/Mhoresa.java
@@ -0,0 +1,10 @@
+package io.github.waduclay;
+
+/**
+ * @author <a href="mailto:willdomkahari@gmail.com">Willdom Kahari</a>
+ */
+public class Mhoresa {
+    public static String mhoresa(String text){
+        return "Urisei ".concat(text);
+    }
+}
